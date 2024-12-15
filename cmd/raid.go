a299aa89ebd072a6497797771b6e44d1313d7ead
Eddie Knight <knight@linux.com>
2024-12-15 16:46:03 -0600

    feat: updated in anticipation of SDK v0.0.16
    Signed-off-by: Eddie Knight <knight@linux.com>

--- cmd/raid.go
+++ cmd/raid.go
@@ -9,13 +9,17 @@ import (
 var (
 	Vessel = raidengine.Vessel{
 		RaidName: "osps-baseline", // Double check that this is what you want the raid to be named
+		Armory:   &armory.Armory,
+		RequiredVars: []string{
+			"repo_url",
+		},
 	} // Used by the plugin or debug function to run the Raid
 )
 
-type Raid struct {}
+type Raid struct{}
 
 // Raid.Start() is called by plugin.Serve
 func (r *Raid) Start() (err error) {
-	err = Vessel.Mobilize(&armory.Armory, nil) // Replace nil with a slice of your required var names
+	err = Vessel.Mobilize() // Replace nil with a slice of your required var names
 	return
 }
